9082df20c4a27fd8155c02c0378b577589875fda
syed <you@example.com>
2025-08-04 02:17:15 +0530

    update cv

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <div className="page-actions">
           <span>Email: syedzaidahmad99@gmail.com</span>
           <span>Mob: +91 8585971473 / +91 8826210811</span>
-          <a href="https://drive.google.com/your-cv-link" target="_blank" rel="noopener noreferrer">CV</a>
+          <a href="https://drive.google.com/file/d/1tyfLAMEaBwdq2_gdJhn9lJ7nAdVsDRug/view?usp=sharing" target="_blank" rel="noopener noreferrer">CV</a>
         </div>
         <Profile />
       </header>
